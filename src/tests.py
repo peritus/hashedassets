340c515bfa762171bd6987b0a1a0e87725e2650c
Filip Noetzel <filip@j03.de>
2010-07-25 11:09:36 +0200

    Clean up tests.py

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,7 @@
-import unittest
-import doctest
 from shutil import rmtree
-
+import doctest
 import os
-
+import unittest
 
 def setUp(dir):
     if os.path.isdir(dir):
@@ -13,11 +11,7 @@ def setUp(dir):
 
 def test_suite():
     setUp('work')
-    suite = unittest.TestSuite(
+    return unittest.TestSuite(
         doctest.DocFileSuite('hashedassets.rst',
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS|doctest.REPORT_UDIFF),
     )
-    return suite
-
-if __name__ == '__main__':
-    unittest.main(defaultTest='test_suite')
