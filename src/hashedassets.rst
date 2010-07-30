830c81118da8e486f6027e10678e57f81d46b240
Filip Noetzel <filip@j03.de>
2010-07-30 20:34:05 +0200

    Move test setup code out of doctest

--- src/hashedassets.rst
+++ src/hashedassets.rst
@@ -1,27 +1,6 @@
 hashedassets
 ============
 
-Preparations
-------------
-
->>> import os
->>> import sys
->>> from subprocess import Popen, STDOUT, PIPE
->>> from shlex import split
-
->>> def system(cmd, env=None):
-...     if env != None:
-...         env.update(os.environ)
-...     process = Popen(split(cmd), stderr=STDOUT, stdout=PIPE, env=env)
-...     # from http://stackoverflow.com/questions/1388753/how-to-get-output-from-subprocess-popen
-...     while True:
-...         out = process.stdout.read(1)
-...         if out == '' and process.poll() != None:
-...             break
-...         if out != '':
-...             sys.stdout.write(out)
-...             sys.stdout.flush()
-
 Creating some source files
 --------------------------
 
--- src/tests.py
+++ src/tests.py
@@ -9,9 +9,33 @@ def setUp(dir):
     os.mkdir(dir)
     os.chdir(dir)
 
+def test_globs():
+    import os
+    import sys
+    from subprocess import Popen, STDOUT, PIPE
+    from shlex import split
+
+    def system(cmd, env=None):
+        if env != None:
+            env.update(os.environ)
+        process = Popen(split(cmd), stderr=STDOUT, stdout=PIPE, env=env)
+        # from http://stackoverflow.com/questions/1388753/how-to-get-output-from-subprocess-popen
+        while True:
+            out = process.stdout.read(1)
+            if out == '' and process.poll() != None:
+                break
+            if out != '':
+                sys.stdout.write(out)
+                sys.stdout.flush()
+
+    return locals()
+
 def test_suite():
     setUp('work')
+
     return unittest.TestSuite(
-        doctest.DocFileSuite('hashedassets.rst',
+        doctest.DocFileSuite(
+            'hashedassets.rst',
+            globs=test_globs(),
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS|doctest.REPORT_UDIFF),
     )
