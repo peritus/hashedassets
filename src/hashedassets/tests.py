d9a180c07f9c8e40761c092e621c7a94fad4a701
Filip Noetzel <filip@j03.de>
2011-01-23 10:51:25 +0100

    Make tests safer (always create a subdir)
    Before that, calling 'python tests.py'
    (without the buildout testrunner) did something
    horrible

--- src/hashedassets/tests.py
+++ src/hashedassets/tests.py
@@ -1,10 +1,17 @@
-from os import listdir, remove
-from os.path import isdir
+from os import listdir, remove, mkdir, chdir
+from os.path import isdir, join, abspath, dirname
 from shutil import rmtree
 import doctest
 import unittest
 
 def setUp():
+    try:
+        mkdir('./__hashedassets_tests')
+    except:
+        pass
+
+    chdir('./__hashedassets_tests')
+
     for afile in listdir('.'):
         if isdir(afile):
             rmtree(afile)
@@ -49,19 +56,23 @@ def test_globs():
     return locals()
 
 def test_suite():
-    setUp()
 
     opts = dict(
+      module_relative=False,
       globs=test_globs(),
       optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS|doctest.REPORT_UDIFF,
     )
 
-    return unittest.TestSuite([
+    suite = unittest.TestSuite([
         doctest.DocTestSuite('hashedassets'),
         doctest.DocTestSuite('hashedassets.rewrite'),
         doctest.DocTestSuite('hashedassets.serializer'),
 
-        doctest.DocFileSuite('hashedassets.rst', **opts),
-        doctest.DocFileSuite('errors.rst', **opts),
-        doctest.DocFileSuite('regressions.rst', **opts),
+        doctest.DocFileSuite(join(dirname(abspath(__file__)), 'hashedassets.rst'), **opts),
+        doctest.DocFileSuite(join(dirname(abspath(__file__)), 'errors.rst'), **opts),
+        doctest.DocFileSuite(join(dirname(abspath(__file__)), 'regressions.rst'), **opts),
     ])
+
+    setUp()
+
+    return suite
